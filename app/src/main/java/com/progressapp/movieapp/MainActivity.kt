10b7cf38b47f518336b544672f184309d89c2fe6
Cristian Gabriel <cristian_granero90@yahoo.com>
2022-11-18 04:25:19 -0300

    navigation controller added, and a animated splash screen

--- app/src/main/java/com/progressapp/movieapp/MainActivity.kt
+++ app/src/main/java/com/progressapp/movieapp/MainActivity.kt
@@ -1,16 +1,35 @@
 package com.progressapp.movieapp
 
 import android.os.Bundle
+import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.progressapp.movieapp.composable.mainscreen.MainScreen
 import com.progressapp.movieapp.ui.theme.MovieAppTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -25,12 +44,77 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    MainScreen()
+                    Navigation()
                 }
             }
         }
     }
 }
+
+@Composable
+fun Navigation(){
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "splash_screen"){
+
+        composable("splash_screen") {
+            SplashScreen(navController = navController)
+        }
+
+        composable("home_screen") {
+            MainScreen()
+        }
+    }
+}
+
+@Composable
+fun SplashScreen(navController: NavController){
+
+    val scale = remember {
+        androidx.compose.animation.core.Animatable(0f)
+    }
+
+    LaunchedEffect(key1 = true ) {
+        scale.animateTo(
+            targetValue = 0.3f,
+            animationSpec = tween(
+                durationMillis = 500,
+                easing = {
+                    OvershootInterpolator(2f).getInterpolation(it)
+                }
+            )
+        )
+        delay(3000L)
+        navController.navigate("home_screen")
+    }
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally
+
+        ) {
+
+            Text(text = "Powered by",
+                fontWeight = FontWeight.Bold,
+                fontStyle = FontStyle.Italic,
+                fontSize = 16.sp)
+
+            Image(
+                painterResource(id = R.drawable.blue_square_splash),
+                contentDescription = "Splash logo",
+                modifier = Modifier
+                    .scale(scale.value)
+                    .fillMaxSize()
+            )
+        }
+    }
+}
+
+
 
 @Composable
 fun Greeting(
