4136d21b52865c5ef885b36bb1c978f23acad8ac
Cristian Gabriel <cristian_granero90@yahoo.com>
2022-11-18 18:31:49 -0300

    Changes in movie screen

--- app/src/main/java/com/progressapp/movieapp/composable/moviescreen/MovieScreen.kt
+++ app/src/main/java/com/progressapp/movieapp/composable/moviescreen/MovieScreen.kt
@@ -1,17 +1,73 @@
 package com.progressapp.movieapp.composable.moviescreen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.progressapp.movieapp.composable.mainscreen.components.BottomBar
+import com.progressapp.movieapp.composable.moviescreen.components.TopBarMovie
+import com.progressapp.movieapp.model.MovieResponse
 
 @Composable
 fun MovieScreen(
-    
+    movieResponse: MovieResponse,
+    backClicked: () -> Unit,
+    accountCliked: () -> Unit,
+    imageUrl: String = "https://image.tmdb.org/t/p/w500",
+    modifier: Modifier = Modifier
 ){
+    Scaffold(
+        modifier = Modifier,
+        topBar = {
+            TopBarMovie(
+                movieTitle = movieResponse.originalTitle,
+                onBackClcked = backClicked,
+                onProfileClicked = accountCliked)
+        },
+        bottomBar = {
+            BottomBar(
+                onHomeClicked = { /*TODO*/ },
+                onMovieClicked = { /*TODO*/ },
+                onFavoritesClicked = { /*TODO*/ },
+                onSearchClicked = { /*TODO*/ })
+        }
+    
+    ) {
+
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.Top
+        ) {
+
+            AsyncImage(
+                model = imageUrl + movieResponse.movieImage,
+                contentDescription = "Movie image",
+                modifier = Modifier.fillMaxWidth()
+            )
+            
+            Text(text = movieResponse.movieDescription, fontSize = 18.sp)
+
+        }
+        
+    }
+    
+    
+
 
 }
 
 @Preview(showBackground = true)
 @Composable
 fun MovieScreenPreview(){
-    MovieScreen()
+    val movie = MovieResponse(false, "20.03.90",20,"Some title", "Some description", "/kqjL17yufvn9OVLyXYpvtyrFfak.jpg")
+    MovieScreen(movie, {}, {})
 }
---
+++ app/src/main/java/com/progressapp/movieapp/composable/moviescreen/components/TopBarMovie.kt
@@ -0,0 +1,57 @@
+package com.progressapp.movieapp.composable.moviescreen.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun TopBarMovie(
+    movieTitle: String,
+    onBackClcked: () -> Unit,
+    onProfileClicked: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        IconButton(onClick = onBackClcked) {
+            Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Go Back")
+        }
+
+        Text(text = movieTitle,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            fontFamily = FontFamily.Cursive,
+            textAlign = TextAlign.Center,
+            )
+
+        IconButton(onClick = onProfileClicked) {
+            Icon(imageVector = Icons.Default.AccountCircle, contentDescription = "Profile settings")
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TopBarMoviePreview(){
+    TopBarMovie("Some title", {}, {})
+
+}
