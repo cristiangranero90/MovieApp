0cecba40d9e64b167d6854e39150194004c792b8
Cristian Gabriel <cristian_granero90@yahoo.com>
2022-11-28 11:01:21 -0300

    BottomBar.kt change to a Bottom navigation

--- app/src/main/java/com/progressapp/movieapp/composable/mainscreen/components/BottomBar.kt
+++ app/src/main/java/com/progressapp/movieapp/composable/mainscreen/components/BottomBar.kt
@@ -1,20 +1,14 @@
 package com.progressapp.movieapp.composable.mainscreen.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun BottomBar(
@@ -24,29 +18,52 @@ fun BottomBar(
     onSearchClicked: () -> Unit,
     modifier: Modifier = Modifier
 ){
-    Row (
-        modifier = Modifier
-            .fillMaxWidth()
-            .background(color = MaterialTheme.colors.background),
-        horizontalArrangement = Arrangement.SpaceAround,
-        verticalAlignment = Alignment.CenterVertically,
-    ){
+    val selectedItem = remember { mutableStateOf(0) }
 
-        IconButton(onClick = onHomeClicked) {
-            Icon(imageVector = Icons.Default.Home, contentDescription = "Home Screen")
-        }
+    BottomNavigation(
+        modifier = Modifier,
+        backgroundColor = MaterialTheme.colors.primary,
+        elevation = 10.dp,
+    ) {
+        BottomNavigationItem(
+            icon = { Icon(imageVector = Icons.Default.Home, contentDescription = "Home Screen") },
+            label = { Text(text = "Home")},
+            selected = (selectedItem.value == 0),
+            onClick = {
+                selectedItem.value = 0
+                onHomeClicked
+            }
+        )
 
-        IconButton(onClick = onMovieClicked) {
-            Icon(imageVector = Icons.Default.PlayArrow, contentDescription = "Movies")
-        }
+        BottomNavigationItem(
+            icon = { Icon(imageVector = Icons.Default.PlayArrow, contentDescription = "Movies screen") },
+            label = { Text(text = "Movies")},
+            selected = (selectedItem.value == 1),
+            onClick = {
+                selectedItem.value = 1
+                onMovieClicked
+            }
+        )
 
-        IconButton(onClick = onFavoritesClicked) {
-            Icon(imageVector = Icons.Default.Star, contentDescription = "Favorites")
-        }
+        BottomNavigationItem(
+            icon = { Icon(imageVector = Icons.Default.Star, contentDescription = "Favorites") },
+            label = { Text(text = "Favourites")},
+            selected = (selectedItem.value == 2),
+            onClick = {
+                selectedItem.value = 2
+                onFavoritesClicked
+            }
+        )
 
-        IconButton(onClick = onSearchClicked) {
-            Icon(imageVector = Icons.Default.Search, contentDescription = "Search movie")
-        }
+        BottomNavigationItem(
+            icon = { Icon(imageVector = Icons.Default.Search, contentDescription = "Search movie") },
+            label = { Text(text = "Search")},
+            selected = (selectedItem.value == 3),
+            onClick = {
+                selectedItem.value = 3
+                onSearchClicked
+            }
+        )
     }
 }
 
