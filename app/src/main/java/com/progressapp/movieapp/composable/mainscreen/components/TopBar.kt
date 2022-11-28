4368295ceed757417953744ce97518a51d915793
Cristian Gabriel <cristian_granero90@yahoo.com>
2022-11-28 10:41:57 -0300

    TopBar.kt and TopBarMovie.kt updated with TopAppBar from scaffold, and the primary colors by default

--- app/src/main/java/com/progressapp/movieapp/composable/mainscreen/components/TopBar.kt
+++ app/src/main/java/com/progressapp/movieapp/composable/mainscreen/components/TopBar.kt
@@ -1,16 +1,15 @@
 package com.progressapp.movieapp.composable.mainscreen.components
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.List
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -24,35 +23,36 @@ fun TopBar(
     onAccountClicked: () -> Unit,
     modifier: Modifier = Modifier
 ){
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .background(color = MaterialTheme.colors.background),
-        horizontalArrangement = Arrangement.SpaceAround,
-        verticalAlignment = Alignment.CenterVertically,
-    ) {
+    TopAppBar(
+        modifier = Modifier,
+        backgroundColor = MaterialTheme.colors.primary,
+        elevation = 10.dp,
 
-        IconButton(onClick = onListClicked) {
-            Icon(imageVector = Icons.Default.List, contentDescription = "Side menu")
-        }
-
-        Box(
-            modifier = Modifier.padding(start = 2.dp, end = 2.dp, top = 4.dp, bottom = 2.dp)
-                .width(300.dp)
-        ) {
+        title = {
             Image(
                 painterResource(id = R.drawable.blue_long_small),
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(end = 10.dp),
                 contentDescription = "Movie image",
                 contentScale = ContentScale.Crop
-                )
-        }
+            )
+        },
 
-        IconButton(onClick = onAccountClicked) {
-            Icon(imageVector = Icons.Default.AccountCircle, contentDescription = "Account settings")
+        navigationIcon = {
+            IconButton(onClick = onListClicked) {
+                Icon(imageVector = Icons.Default.List, contentDescription = "Side menu")
+            }
+        },
+
+        actions = {
+            IconButton(onClick = onAccountClicked) {
+                Icon(imageVector = Icons.Default.AccountCircle, contentDescription = "Account settings")
+            }
         }
-    }
+    )
 }
+
 
 @Preview(showBackground = true)
 @Composable
--- app/src/main/java/com/progressapp/movieapp/composable/moviescreen/components/TopBarMovie.kt
+++ app/src/main/java/com/progressapp/movieapp/composable/moviescreen/components/TopBarMovie.kt
@@ -1,20 +1,11 @@
 package com.progressapp.movieapp.composable.moviescreen.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -30,31 +21,34 @@ fun TopBarMovie(
     onProfileClicked: () -> Unit,
     modifier: Modifier = Modifier
 ){
-    Row(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
+    TopAppBar(
+        modifier = Modifier,
+        backgroundColor = MaterialTheme.colors.primary,
+        elevation = 10.dp,
 
-        IconButton(onClick = onBackClcked) {
-            Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Go Back")
-        }
-
-        Text(text = movieTitle,
-            fontSize = 24.sp,
-            fontWeight = FontWeight.Bold,
-            fontFamily = FontFamily.Cursive,
-            textAlign = TextAlign.Center,
-            overflow = TextOverflow.Ellipsis,
-            maxLines = 1,
-            modifier = Modifier.width(300.dp)
+        title = {
+            Text(text = movieTitle,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.Cursive,
+                textAlign = TextAlign.Center,
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 1,
             )
+        },
 
-        IconButton(onClick = onProfileClicked) {
-            Icon(imageVector = Icons.Default.AccountCircle, contentDescription = "Profile settings")
+        navigationIcon = {
+            IconButton(onClick = onBackClcked) {
+                Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Go Back")
+            }
+        },
+
+        actions = {
+            IconButton(onClick = onProfileClicked) {
+                Icon(imageVector = Icons.Default.AccountCircle, contentDescription = "Profile settings")
+            }
         }
-
-    }
+    )
 }
 
 @Preview(showBackground = true)
